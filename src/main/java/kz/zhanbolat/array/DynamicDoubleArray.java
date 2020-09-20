23d20649b259d13438b72c79fba4373a38a108b3
Zhanbolat Orakbayev <fous363@gmail.com>
2020-09-20 21:07:23 +0600

    Impl Add functionality

---
+++ src/main/java/kz/zhanbolat/array/DynamicDoubleArray.java
@@ -0,0 +1,35 @@
+package kz.zhanbolat.array;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class DynamicDoubleArray {
+    private double[] array;
+    private int capacity;
+    private int count;
+
+    public DynamicDoubleArray() {
+        array = new double[0];
+    }
+
+    public DynamicDoubleArray(int capacity) {
+        array = new double[0];
+        this.capacity = capacity;
+    }
+
+    public boolean add(double value) {
+        if (array.length == 0) {
+            array = new double[1];
+            array[0] = value;
+        }
+        if (array.length == capacity) {
+            return false;
+        }
+        double[] buffer = Arrays.copyOf(array, array.length);
+        array = new double[array.length + 1];
+        System.arraycopy(buffer, 0, array, 0, buffer.length);
+        array[array.length - 1] = value;
+        return true;
+    }
+
+}
